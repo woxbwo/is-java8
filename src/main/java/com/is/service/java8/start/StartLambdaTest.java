44c9542bffd1ea66153ec61454bc4293bcc2404e
woxbwo <woxbwo@163.com>
2019-08-13 19:22:05 +0800

    add code

--- src/main/java/com/is/service/java8/start/StartLambdaTest.java
+++ src/main/java/com/is/service/java8/start/StartLambdaTest.java
@@ -1,9 +1,9 @@
 package com.is.service.java8.start;
 
+import com.is.service.java8.model.Employee;
 import org.junit.Test;
 
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @ClassName: StartLambdaTest
@@ -32,4 +32,75 @@ public class StartLambdaTest {
             System.out.printf(integer+"");
         }
     }
+
+    /**
+     *@description:
+     * Lambda 表达式
+     *@params:  []
+     *@return:  void
+     **/
+    @Test
+    public void test2(){
+        Comparator<Integer> comparator = (x,y)->Integer.compare(x,y);
+        TreeSet<Integer> treeSet = new TreeSet<>(comparator);
+    }
+
+    //数组转list
+    List<Employee> employees = Arrays.asList(
+            new Employee(1,"zbbiex1",83,4444.99),
+            new Employee(2,"zbbiex2",83,5555.99),
+            new Employee(3,"zbbiex3",27,3333.99),
+            new Employee(4,"zbbiex4",24,7777.99),
+            new Employee(5,"zbbiex5",36,8888.99),
+            new Employee(6,"zbbiex6",45,6666.99)
+
+    );
+
+    /**
+     *@description:
+     *@params: 查询员工年龄大于35的
+     *@return:  void
+     **/
+    @Test
+    public void test3(){
+        List<Employee> employees = filterEmpByAge(this.employees);
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+    List<Employee> filterEmpByAge(List<Employee> emps){
+        List<Employee> res = new ArrayList<>();
+        for (Employee employee : emps) {
+            if(employee.getAge()>35){
+                res.add(employee);
+            }
+        }
+        return res;
+    }
+
+    /**
+     *@description:
+     * 查询工资大于5000的员工
+     *@params:  []
+     *@return:  void
+     **/
+    @Test
+    public void test4(){
+        List<Employee> res = filterEmpBySalary(this.employees);
+        for (Employee employee : res) {
+            System.out.println(employee);
+        }
+    }
+    List<Employee> filterEmpBySalary(List<Employee> emps){
+        List<Employee> res = new ArrayList<>();
+        for (Employee employee : emps) {
+            if(employee.getSalary()>5000){
+                res.add(employee);
+            }
+        }
+        return res;
+    }
+
+
+
 }
